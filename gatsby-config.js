9788c1ff14cef32b2e9759ba54445110ae79f8f5
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-01-28 05:01:57 +0800

    remove gatsby offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
